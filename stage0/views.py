de19a08a3220b6d3f239074db4b413913ca54e97
yanmife.at@gmail.com <oatewogbola@hawk.iit.edu>
2025-01-30 11:38:08 -0500

    updated git

--- stage0/views.py
+++ stage0/views.py
@@ -5,7 +5,7 @@ def my_info(request):
     response_data = {
         "email": "yanmife.at@gmail.com",
         "current_datetime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
-        "github_url": "https://github.com/"
+        "github_url": "https://github.com/Miife/HNG12"
     }
     return JsonResponse(response_data, json_dumps_params={"indent": 2})
 
